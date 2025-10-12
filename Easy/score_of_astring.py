a297ae009db7bcd3ceedd47fbe3059ca12b25afe
acatrem <oguzhankr123@gmail.com>
2025-10-12 12:14:41 +0300

    Add new solution: Score of a String

---
+++ Easy/score_of_astring.py
@@ -0,0 +1,19 @@
+# Problem : https://leetcode.com/problems/score-of-a-string/
+# Difficulty : Easy
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+
+def scoreS(str):
+    score = 0
+    if(len(str) > 2 and len(str) <100 and str.islower()):
+        for i in range(0, len(str)-1):
+                score += abs(ord(str[i]) - ord(str[i+1])) 
+                # ord() fonksiyonu Python'da kendisine verilen tek bir karakterin sayısal karşılığını (Unicode/ASCII kodunu) döndürür. 
+        print(score)
+            
+    else:
+        print("Geçersiz giriş. Lütfen 2 ile 1000 karakter arasinda ve sadece küçük harflerden oluşan bir string girin.")
+
+s = input("s = ")
+
+scoreS(s)
